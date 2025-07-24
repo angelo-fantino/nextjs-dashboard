760fb7a709ec154d339ecc71fdd60670a474569d
Angelo One <fantino.angelo@gmail.com>
2025-07-24 07:14:44 +0100

    Revert "style(lib): Replace single quotes with double quotes."
    This reverts commit 698e99a7d480a070091969065db6090db2aaf202.

--- app/ui/dashboard/cards.tsx
+++ app/ui/dashboard/cards.tsx
@@ -3,9 +3,8 @@ import {
   ClockIcon,
   UserGroupIcon,
   InboxIcon,
-} from "@heroicons/react/24/outline";
-import { lusitana } from "@/app/ui/fonts";
-import { fetchCardData } from "@/app/lib/data";
+} from '@heroicons/react/24/outline';
+import { lusitana } from '@/app/ui/fonts';
 
 const iconMap = {
   collected: BanknotesIcon,
@@ -15,22 +14,18 @@ const iconMap = {
 };
 
 export default async function CardWrapper() {
-  const {
-    numberOfInvoices,
-    numberOfCustomers,
-    totalPaidInvoices,
-    totalPendingInvoices,
-  } = await fetchCardData();
   return (
     <>
-      <Card title="Collected" value={totalPaidInvoices} type="collected" />
+      {/* NOTE: Uncomment this code in Chapter 9 */}
+
+      {/* <Card title="Collected" value={totalPaidInvoices} type="collected" />
       <Card title="Pending" value={totalPendingInvoices} type="pending" />
       <Card title="Total Invoices" value={numberOfInvoices} type="invoices" />
       <Card
         title="Total Customers"
         value={numberOfCustomers}
         type="customers"
-      />
+      /> */}
     </>
   );
 }
@@ -42,16 +37,14 @@ export function Card({
 }: {
   title: string;
   value: number | string;
-  type: "invoices" | "customers" | "pending" | "collected";
+  type: 'invoices' | 'customers' | 'pending' | 'collected';
 }) {
   const Icon = iconMap[type];
 
   return (
     <div className="rounded-xl bg-gray-50 p-2 shadow-sm">
       <div className="flex p-4">
-        {/* start defensive coding practice */}
         {Icon ? <Icon className="h-5 w-5 text-gray-700" /> : null}
-        {/* end defensive coding practice */}
         <h3 className="ml-2 text-sm font-medium">{title}</h3>
       </div>
       <p
